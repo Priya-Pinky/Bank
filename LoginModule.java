e531abbbc0b5fbfb2740c62f114c5831b85b3fd8
Priya-Pinky <chepuripriyanka1110@gmail.com>
2025-10-08 12:49:11 +0530

    Update LoginModule.java

--- LoginModule.java
+++ LoginModule.java
@@ -3,5 +3,6 @@ public class Bank
     public static void main(String[] args)
     {
     System.out.println("Creating Login Module");
+     System.out.println("Creating seconds Module in central");
   }
   }
